4a1674718eaa73415993894ed9ebf82fc723b035
HelLuv <works.person@gmail.com>
2022-05-02 17:20:30 +0300

    added liking and deleting logic to post component

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -18,6 +18,7 @@ import {useRecoilState} from "recoil";
 import {modalState, postIdState} from "../../atoms/modalAtom";
 import {db} from "../../firebase"
 import {useRouter} from "next/router";
+import {collection, deleteDoc, doc, onSnapshot, orderBy, query, setDoc} from "@firebase/firestore";
 
 interface PostProps {
   id: string;
@@ -29,26 +30,63 @@ const Post: React.FC<PostProps> = ({post, id, isPostPage}) => {
   const {data: session} = useSession();
   const [isOpen, setIsOpen] = useRecoilState(modalState);
   const [postId, setPostId] = useRecoilState(postIdState);
-  const [comments, setComments] = React.useState([]);
-  const [likes, setLikes] = React.useState([]);
+  const [comments, setComments] = React.useState<any[]>([]);
+  const [likes, setLikes] = React.useState<any[]>([]);
   const [isLiked, setIsLiked] = React.useState(false);
 
   const router = useRouter();
+
+  React.useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [db, id]
+  )
+
+  React.useEffect(
+    () =>
+      onSnapshot(collection(db, "posts", id, "likes"), (snapshot) =>
+        setLikes(snapshot.docs)
+      ),
+    [db, id]
+  );
+
+  React.useEffect(
+    () =>
+      setIsLiked(
+        likes.findIndex((like: any) => like.id === session?.user?.uid) !== -1
+      ), [likes])
 
   const handlePostOpen = () => {
     router.push(`/${id}`)
   };
 
-  const handlePostComment = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const handleCommentPost = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.stopPropagation();
   };
 
-  const handlePostDelete = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const handleDeletePost = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.stopPropagation();
+
+    deleteDoc(doc(db, "posts", id));
+    router.push("/");
   };
 
-  const handlePostLike = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const handleLikePost = async (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.stopPropagation();
+
+    if (isLiked) {
+      await deleteDoc(doc(db, "posts", id, "likes", session!.user.uid));
+    } else {
+      await setDoc(doc(db, "posts", id, "likes", session!.user.uid), {
+        username: session?.user.name,
+      });
+    }
   };
 
   return (
@@ -106,16 +144,18 @@ const Post: React.FC<PostProps> = ({post, id, isPostPage}) => {
             {post?.text}
           </p>
         )}
-        <img
-          src={post?.image}
-          alt="post image"
-          className="rounded-2xl max-h-[700px] object-cover mr-2"
-        />
+        {post?.image &&
+            <img
+                src={post?.image}
+                alt="post image"
+                className="rounded-2xl max-h-[700px] object-cover mr-2"
+            />
+        }
         <div className={`text-[#6e767d] flex justify-between w-10/12 ${isPostPage && "mx-auto"}`}
         >
           <div
             className="flex items-center space-x-1 group"
-            onClick={(e) => handlePostComment(e)}
+            onClick={(e) => handleCommentPost(e)}
           >
             <div className="icon group-hover:bg-[#1d9bf0] group-hover:bg-opacity-10">
               <ChatIcon className="h-5 group-hover:text-[#1d9bf0]"/>
@@ -131,7 +171,7 @@ const Post: React.FC<PostProps> = ({post, id, isPostPage}) => {
           {session?.user.uid === post?.id ? (
             <div
               className="flex items-center space-x-1 group"
-              onClick={(e) => handlePostDelete(e)}
+              onClick={(e) => handleDeletePost(e)}
             >
               <div className="icon group-hover:bg-red-600/10">
                 <TrashIcon className="h-5 group-hover:text-red-600"/>
@@ -147,7 +187,7 @@ const Post: React.FC<PostProps> = ({post, id, isPostPage}) => {
 
           <div
             className="flex items-center space-x-1 group"
-            onClick={(e) => handlePostLike(e)}
+            onClick={(e) => handleLikePost(e)}
           >
             <div className="icon group-hover:bg-pink-600/10">
               {isLiked ? (
